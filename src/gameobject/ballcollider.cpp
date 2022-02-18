683cfeebe1cde635ca4ac357bc39afad02e347fa
KJPOLWIN <85358420+KJPOLWIN@users.noreply.github.com>
2022-02-18 18:35:17 +0100

    Collision detection change

--- src/gameobject/ballcollider.cpp
+++ src/gameobject/ballcollider.cpp
@@ -2,6 +2,8 @@
 #include "constants.h"
 #include <SFML/Graphics.hpp>
 
+    #include <iostream>
+
 BallCollider::BallCollider(sf::Vector2f position, sf::Vector2f size)
   : hitbox{ size }
 {
@@ -10,22 +12,30 @@ BallCollider::BallCollider(sf::Vector2f position, sf::Vector2f size)
 
 bool BallCollider::checkForCollisions(Ball& ball)
 {
-  return (((ball.getPosition().x + constant::ballRadius > hitbox.getPosition().x
-  && ball.getPosition().x + constant::ballRadius < hitbox.getPosition().x + hitbox.getSize().x)
-  || (ball.getPosition().x + constant::ballRadius < hitbox.getPosition().x
-  && ball.getPosition().x + constant::ballRadius > hitbox.getPosition().x + hitbox.getSize().x))
-  || ((ball.getPosition().x - constant::ballRadius > hitbox.getPosition().x
-  && ball.getPosition().x - constant::ballRadius < hitbox.getPosition().x + hitbox.getSize().x)
-  || (ball.getPosition().x - constant::ballRadius < hitbox.getPosition().x
-  && ball.getPosition().x - constant::ballRadius > hitbox.getPosition().x + hitbox.getSize().x)))
-  && (((ball.getPosition().y + constant::ballRadius > hitbox.getPosition().y
-  && ball.getPosition().y + constant::ballRadius < hitbox.getPosition().y + hitbox.getSize().y)
-  || (ball.getPosition().y + constant::ballRadius < hitbox.getPosition().y
-  && ball.getPosition().y + constant::ballRadius > hitbox.getPosition().y + hitbox.getSize().y))
-  || ((ball.getPosition().y - constant::ballRadius > hitbox.getPosition().y
-  && ball.getPosition().y - constant::ballRadius < hitbox.getPosition().y + hitbox.getSize().y)
-  || (ball.getPosition().y - constant::ballRadius < hitbox.getPosition().y
-  && ball.getPosition().y - constant::ballRadius > hitbox.getPosition().y + hitbox.getSize().y)));
+  sf::Vector2f size = hitbox.getSize();
+  sf::Vector2f hpos = hitbox.getPosition();
+  sf::Vector2f bpos = ball.getPosition();
+  double rad = constant::ballRadius;
+
+  //ball hits from the bottom
+  return (((bpos.y > hpos.y + size.y) && (hpos.y + size.y > bpos.y - rad)
+        && (bpos.x > hpos.x) && (bpos.x < hpos.x + size.x))
+
+  //ball hits from the left
+  || ((bpos.x < hpos.x) && (bpos.x + rad > hpos.x)
+  && (bpos.y > hpos.y) && (bpos.y < hpos.y + size.y))
+
+  //ball hits from the right
+  || ((bpos.x > hpos.x + size.x) && (bpos.x - rad < hpos.x + size.x)
+    && (bpos.y > hpos.y) && (bpos.y < hpos.y + size.y))
+
+  //ball hits from the top
+  || ((bpos.y < hpos.y) && (hpos.y < bpos.y + rad)
+  && (bpos.x > hpos.x) && (bpos.x < hpos.x + size.x)));
+
+  //ball is inside
+  //|| ((bpos.x > hpos.x) && (bpos.x < hpos.x + size.x) && (bpos.y > hpos.y) && (bpos.y < hpos.y + size.y)));
+
 }
 
 void BallCollider::setColor(sf::Color color)
